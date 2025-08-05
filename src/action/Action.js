563cbcadd8f137cd979629a37028d683e0933313
Phanindra <tphanindra28@gmail.com>
2025-08-05 21:05:04 +0530

    Add News letter section

--- src/action/Action.js
+++ src/action/Action.js
@@ -1,15 +1,17 @@
 export const handleFormSubmit = (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const form = event.target;
+  const form = event.target;
 
-    const email = form.elements["email"].value;
+  const email = form.elements["email"].value;
 
-    if(email){
-        fetch('https://www.greatfrontend.com/api/projects/challenges/newsletter\n', {
-            method: 'POST',
-            body: JSON.stringify({ email }),
-        })
-    }
-
+  if (email) {
+    fetch(
+      "https://www.greatfrontend.com/api/projects/challenges/newsletter\n",
+      {
+        method: "POST",
+        body: JSON.stringify({ email }),
+      },
+    );
+  }
 };
